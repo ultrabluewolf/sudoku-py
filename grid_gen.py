2cb4429b80f8c65df1d0cdc6349d44d01bc0b394
Britney L <ultrabluewolf-fb2@yahoo.com>
2013-07-26 11:01:29 -0400

    continuing randomize solution generator

--- grid_gen.py
+++ grid_gen.py
@@ -9,16 +9,22 @@ class GridGen:
 	HARD=5
 	def __init__(self,diff_lvl):
 		self.lvl=diff_lvl
+		self.rand = Random()
 	
 	def create(self):
 		g=Grid(None)
 		return g
 	
+	"""takes randomized solution and clears numbers depending on difficulty selected"""
+	def poke_holes(self):
+		pass
+	
 	def gen_randomized_soln(self):
+		self.rand.seed()
 		for row in grid.MAX:
 			usedNums=set()
 			for col in grid.MAX:
-				num = rand...
+				num = self.rand.randint(1,grid.MAX)
 				if not num in usedNums:
 					usedNums.add(num)
 					
--- tests.py
+++ tests.py
@@ -3,7 +3,12 @@
 import engine, checker, grid, grid_gen, utils
 from grid import *
 
-grids=utils.readGridInput('input')
-for i in range(len(grids)-1):
-	print str(grids[i])
-	print checker.is_valid(grids[i])
+if False:
+	grids=utils.readGridInput('input')
+	for i in range(len(grids)-1):
+		print str(grids[i])
+		print checker.is_valid(grids[i])
+
+if True:
+	grids=GridGen(1).create()
+	
